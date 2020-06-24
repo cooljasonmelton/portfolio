34654d4f2fe2c25df96aedb7454adb5823da0bd7
cooljasonmelton <cooljasonmelton>
2020-06-24 15:16:07 -0500

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar'
+
 
 function App() {
   return (
     <div className="app">
+      <Navbar/>
 
     </div>
   );
---
+++ src/Navbar.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import { Menu, Segment } from 'semantic-ui-react'
+
+export default class Navbar extends Component {
+  state = { activeItem: 'home' }
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+  render() {
+    const { activeItem } = this.state
+
+    return (
+      <div>
+        <Menu pointing secondary>
+          <Menu.Item
+            name='jason melton'
+            active={activeItem === 'jason melton'}
+            onClick={this.handleItemClick}
+            style={{fontWeight: '900'}}
+          />
+        <Menu.Menu position='right'>
+          <Menu.Item 
+            name='projects'
+            active={activeItem === 'projects'}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item 
+            name='about'
+            active={activeItem === 'about'}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item 
+            name='contact'
+            active={activeItem === 'contact'}
+            onClick={this.handleItemClick}
+          />
+            <Menu.Item
+              name='blog'
+              active={activeItem === 'blog'}
+              onClick={this.handleItemClick}
+            />
+          </Menu.Menu>
+        </Menu>
+      </div>
+    )
+  }
+}
